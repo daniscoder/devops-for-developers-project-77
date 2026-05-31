6d069440c3651e183b785bbf190b67e5cb6b3361
Danis Arslanov <arslanovdk@gmail.com>
2026-05-31 19:51:28 -0400

    Add domain, SSL certificate and complete infrastructure

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-### Hexlet tests and linter status:
-[![Actions Status](https://github.com/daniscoder/devops-for-developers-project-77/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/daniscoder/devops-for-developers-project-77/actions)
+# Devops For Developers Project 77
+
+Развёртывание инфраструктуры через Terraform и деплой приложения через Ansible.
+
+## Приложение
+
+https://daniscoder.online
+
+## Требования
+
+* Terraform
+* Ansible
+* Make
+
+## Инициализация Terraform
+
+```bash
+make init SPACES_ACCESS_KEY=<key> SPACES_SECRET_KEY=<secret>
+```
+
+## Создание инфраструктуры
+
+```bash
+make apply DO_TOKEN=<token>
+```
+
+## Установка зависимостей Ansible
+
+```bash
+make install
+```
+
+## Подготовка серверов
+
+```bash
+make setup
+```
+
+## Деплой приложения
+
+```bash
+make deploy
+```
+
+## Удаление инфраструктуры
+
+```bash
+make destroy DO_TOKEN=<token>
+```
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,30 @@ data "digitalocean_ssh_key" "default" {
 }
 
 resource "digitalocean_droplet" "web" {
-  count  = 2
-  name   = "web-tf-${count.index + 1}"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-24-04-x64"
-  region = "nyc1"
-  tags   = ["web"]
+  count    = 2
+  name     = "web-tf-${count.index + 1}"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-24-04-x64"
+  region   = "nyc1"
+  tags     = ["web"]
   ssh_keys = [data.digitalocean_ssh_key.default.id]
+}
+
+resource "digitalocean_domain" "default" {
+  name = "daniscoder.online"
+}
+
+resource "digitalocean_record" "default" {
+  domain = digitalocean_domain.default.name
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_loadbalancer.web.ip
+}
+
+resource "digitalocean_certificate" "default" {
+  name    = "daniscoder-online-cert"
+  type    = "lets_encrypt"
+  domains = ["daniscoder.online"]
 }
 
 resource "digitalocean_loadbalancer" "web" {
@@ -22,6 +39,14 @@ resource "digitalocean_loadbalancer" "web" {
     entry_protocol  = "http"
     target_port     = 80
     target_protocol = "http"
+  }
+
+  forwarding_rule {
+    entry_port       = 443
+    entry_protocol   = "https"
+    target_port      = 80
+    target_protocol  = "http"
+    certificate_name = digitalocean_certificate.default.name
   }
 
   healthcheck {

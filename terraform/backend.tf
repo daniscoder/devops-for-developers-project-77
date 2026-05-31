cae610a9a48685e67499c7d73e08b7f45af7ee61
Danis Arslanov <arslanovdk@gmail.com>
2026-05-31 17:57:49 -0400

    Add Terraform base configuration with remote backend

---
+++ terraform/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    endpoints = {
+      s3 = "https://nyc3.digitaloceanspaces.com"
+    }
+    region                      = "us-east-1"
+    bucket                      = "daniscoder-terraform-state"
+    key                         = "terraform.tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+    skip_requesting_account_id  = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "do_token" {
+  description = "DigitalOcean API token"
+  sensitive   = true
+}
+
+variable "spaces_access_key" {
+  description = "Spaces access key"
+  sensitive   = true
+}
+
+variable "spaces_secret_key" {
+  description = "Spaces secret key"
+  sensitive   = true
+}
